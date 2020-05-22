d98183a9cdb575f63bf314432227c56b6e367b45
unknown <william.z.wilson95@gmail.com>
2020-05-22 11:56:28 -0500

    done

--- README.md
+++ README.md
@@ -26,10 +26,10 @@ In this project you will create a login page and request a token from the server
 
 Demonstrate your understanding of this Sprint's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager.
 
-- [ ] Explain what a token is used for.
-- [ ] What steps can you take in your web apps to keep your data secure?
-- [ ] Describe how web servers work.
-- [ ] Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+- [ ] Explain what a token is used for. for authentication
+- [ ] What steps can you take in your web apps to keep your data secure? implement private routes
+- [ ] Describe how web servers work. a server uses http and different protocols to respond to client requests
+- [ ] Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers. post, get, put, delete??
 
 
 ## Project Set Up
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWIthAuth";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  const [creds, setCreds] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChanges = (e) => {
+    setCreds({
+      ...creds,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  let history = useHistory();
+
+  const login = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", creds)
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+        history.push("/protected");
+      });
+  };
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div className="logins">
+        <form onSubmit={login}>
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            value={creds.username}
+            onChange={handleChanges}
+          />
+
+          <br />
+          <br />
+
+          <input
+            type="text"
+            placeholder="password"
+            name="password"
+            value={creds.password}
+            onChange={handleChanges}
+          />
+          <button>submit</button>
+        </form>
+      </div>
     </>
   );
 };
